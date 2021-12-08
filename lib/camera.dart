e93e711a7a585942e1075d66361ae59ebcc68f2b
bekirin50tonu <50025610+bekirin50tonu@users.noreply.github.com>
2021-12-08 08:45:40 +0300

    Update camera.dart

--- lib/camera.dart
+++ lib/camera.dart
@@ -18,11 +18,13 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
   CameraController? _controller = null;
   int _selected = 0;
   String res = "";
-  List<dynamic>? predictions = [];
+  List<dynamic>? predictions;
   String? output = "";
   double confidence = 0.0;
   CameraImage? img;
   bool isProgress = false;
+  double _imageHeight = 0.0;
+  double _imageWidth = 0.0;
 
   @override
   void initState() {
@@ -55,18 +57,21 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: Text('Camera'),
       ),
       body: Container(
-          child: Column(children: [
+          child: Stack(children: [
         _controller == null
             ? widget.loadingWidget
-            : CameraPreview(_controller!),
-        Text(predictions!.length.toString()),
-        Text(confidence.toString()),
-        Text(output ?? "Bulunamadı"),
+            : SizedBox(
+                child: CameraPreview(_controller!),
+                height: size.height,
+                width: size.width,
+              ),
+        renderBoxes(size).first
       ])),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -90,7 +95,12 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
     if (!mounted) {
       return;
     } else {
-      _controller!.startImageStream((image) => {img = image, recognitions()});
+      _controller!.startImageStream((image) => {
+            img = image,
+            recognition(),
+            _imageWidth = img!.width.toDouble(),
+            _imageHeight = img!.height.toDouble()
+          });
     }
   }
 
@@ -118,11 +128,11 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
         "unsuccess";
   }
 
-  Future<void> recognitions() async {
+  recognition() async {
     if (img == null) return;
     if (isProgress) return;
     isProgress = true;
-    var recognitions = await Tflite.runModelOnFrame(
+    var recognitions = await Tflite.detectObjectOnFrame(
         bytesList: img!.planes.map((plane) {
           return plane.bytes;
         }).toList(), // required
@@ -131,7 +141,6 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
         imageMean: 127.5, // defaults to 127.5
         imageStd: 127.5, // defaults to 127.5
         rotation: 90, // defaults to 90, Android only
-        numResults: 1, // defaults to 5
         threshold: 0.5, // defaults to 0.1
         asynch: true // defaults to true
         );
@@ -139,12 +148,43 @@ class _CameraState extends State<Camera> with WidgetsBindingObserver {
 
     var xd = recognitions!.map((e) => print(e['rect']));
     if (recognitions == null) return;
-    recognitions.forEach((element) => {
-          setState(() => {
-                output = element['label'],
-                confidence = element['confidence'],
-                print(element)
-              })
-        });
+    predictions = recognitions;
+  }
+
+  List<Widget> renderBoxes(Size screen) {
+    if (predictions == null) return [Text('nope')];
+    if (_imageWidth == null || _imageHeight == null) return [];
+
+    double factorX = screen.width;
+    double factorY = _imageHeight / _imageHeight * screen.width;
+
+    Color blue = Colors.blue;
+
+    return predictions!.map((re) {
+      return Container(
+        child: Positioned(
+            left: re["rect"]["x"] * factorX,
+            top: re["rect"]["y"] * factorY,
+            width: re["rect"]["w"] * factorX,
+            height: re["rect"]["h"] * factorY,
+            child: ((re["confidenceInClass"] > 0.50))
+                ? Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: blue,
+                      width: 3,
+                    )),
+                    child: Text(
+                      "${re["detectedClass"]} ${(re["confidenceInClass"] * 100).toStringAsFixed(0)}%",
+                      style: TextStyle(
+                        background: Paint()..color = blue,
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  )
+                : Container()),
+      );
+    }).toList();
   }
 }
